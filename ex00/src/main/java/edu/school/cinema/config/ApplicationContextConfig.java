d81ca2601962f66034214904bc4c1d031ff83897
ssoraka <ssoraka@student.21-school.ru>
2022-08-28 21:37:20 +0300

    fix

--- ex00/src/main/java/edu/school/cinema/config/ApplicationContextConfig.java
+++ ex00/src/main/java/edu/school/cinema/config/ApplicationContextConfig.java
@@ -2,7 +2,6 @@ package edu.school.cinema.config;
 
 import edu.school.cinema.repositories.UserDao;
 import edu.school.cinema.repositories.UserDaoImpl;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,15 +12,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = { "edu.school.cinema" })
